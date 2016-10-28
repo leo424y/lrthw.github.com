89085616f6184323883f97776de6538ce26dd2db
leo424y <leo424y@gmail.com>
2016-10-28 16:12:36 +0800

    ex09.md

---
+++ ex09/ex09.rb
@@ -0,0 +1,14 @@
+days = 'Mon Tue Wed Thu Fri Sat Sun'
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+puts 'Here are the days: ', days
+puts 'Here are the months', months
+
+puts <<PARAGRAPH
+lsdjfal;sjdfl;jas;ldfj
+al;sdjfalksdfj
+alks;dfjal;sdjflasjd;lffasdf
+weruqweruqwourioqwupreou
+PARAGRAPH
+
+# 自己檢查結果，記錄你犯過的錯誤，並且在下個練習中儘量不犯相同的錯誤。
