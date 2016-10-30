d95e22c0121f2f6662851ea42cf099e442d6a558
leo424y <leo424y@gmail.com>
2016-10-30 12:26:16 +0800

    ex13

---
+++ ex13/ex13.rb
@@ -0,0 +1,6 @@
+first, second, third = ARGV # 解包 unpack
+
+puts "The script is called:  #{$PROGRAM_NAME}" # $0 > $PROGRAM_NAME
+puts "Your first variable is: #{first}"
+puts "Your second variable is: #{second}"
+puts "Your third variable is: #{third}"
