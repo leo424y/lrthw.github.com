b43635f89a1d6eddfd2eb9a4cc7851d63030220b
leo424y <leo424y@gmail.com>
2016-10-30 12:40:21 +0800

    ex13y

--- ex13/ex13b.rb
+++ ex13/ex13b.rb
@@ -1,14 +1,10 @@
-
-print 'Input your sex: '
-sex = gets.chomp
-
-first, second, third = ARGV # 解包 unpack
+first, second, third, fourth = ARGV # 解包 unpack
 puts "The script is called:  #{$PROGRAM_NAME}" # $0 > $PROGRAM_NAME
 puts "Your first variable is: #{first}"
 puts "Your second variable is: #{second}"
 puts "Your third variable is: #{third}"
-puts "Your sex variable is: #{sex}"
-#
+puts "Your fourth variable is: #{fourth}"
+
 # 傳三個以下的參數給你的腳本。當有缺少參數時哪些數值會被使用到？ 先進先出
 # 再寫兩個腳本，其中一個接收更少的參數，另一個接收更多的參數。在參數解包時給它們取一些有意義的變數名稱。
 # 結合 gets.chomp 和 ARGV 一起使用，讓你的腳本從用戶手上得到更多輸入。
---
+++ ex13/ex13d.rb
@@ -0,0 +1,16 @@
+first, second, third, fourth = ARGV # 解包 unpack
+print 'Input your sex: '
+sex = STDIN.gets.chomp
+puts "Hello, #{sex}!"
+
+puts "The script is called:  #{$PROGRAM_NAME}" # $0 > $PROGRAM_NAME
+puts "Your first variable is: #{sex}#{first}"
+puts "Your second variable is: #{sex}#{second}"
+puts "Your third variable is: #{sex}#{third}"
+puts "Your fourth variable is: #{sex}#{fourth}"
+
+# 傳三個以下的參數給你的腳本。當有缺少參數時哪些數值會被使用到？ 先進先出
+# 再寫兩個腳本，其中一個接收更少的參數，另一個接收更多的參數。在參數解包時給它們取一些有意義的變數名稱。
+# 結合 gets.chomp 和 ARGV 一起使用，讓你的腳本從用戶手上得到更多輸入。
+# No such file or directory @ rb_sysopen - 1 (Errno::ENOENT)
+# http://stackoverflow.com/questions/27453569/using-gets-gives-no-such-file-or-directory-error-when-i-pass-arguments-to-my/27453657
