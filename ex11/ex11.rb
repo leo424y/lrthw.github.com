f5c776d058f9e718b427c07aca6010613b7a0286
leo424y <leo424y@gmail.com>
2016-10-30 12:57:32 +0800

    ex13d

--- ex11/ex11.rb
+++ ex11/ex11.rb
@@ -5,3 +5,5 @@ puts "Hello, #{sex}!"
 # http://lrthw.github.io/ex11/
 # gets 取資料
 # chomp 去掉字串末的輸入
+# sex = STDIN.gets.chomp
+# This is documented as IO.gets. STDIN is an instance of IO.
--- ex13/ex13d.rb
+++ ex13/ex13d.rb
@@ -1,14 +1,13 @@
 first, second, third, fourth = ARGV # 解包 unpack
-print 'Input your sex: '
-sex = STDIN.gets.chomp
-puts "Hello, #{sex}!"
+print 'Input your first word: '
+firstword = STDIN.gets.chomp # $
+puts "Hello, #{firstword}!"
 
 puts "The script is called:  #{$PROGRAM_NAME}" # $0 > $PROGRAM_NAME
-puts "Your first variable is: #{sex}#{first}"
-puts "Your second variable is: #{sex}#{second}"
-puts "Your third variable is: #{sex}#{third}"
-puts "Your fourth variable is: #{sex}#{fourth}"
-
+puts "Your first variable is: #{firstword}#{first}"
+puts "Your second variable is: #{firstword}#{second}"
+puts "Your third variable is: #{firstword}#{third}"
+puts "Your fourth variable is: #{firstword}#{fourth}"
 # 傳三個以下的參數給你的腳本。當有缺少參數時哪些數值會被使用到？ 先進先出
 # 再寫兩個腳本，其中一個接收更少的參數，另一個接收更多的參數。在參數解包時給它們取一些有意義的變數名稱。
 # 結合 gets.chomp 和 ARGV 一起使用，讓你的腳本從用戶手上得到更多輸入。
