4e52ca49bd51a04998f1cce6a852a761bac4ca52
leo424y <leo424y@gmail.com>
2016-11-03 16:40:07 +0800

    ex29 gogo

--- ex29/ex29.rb
+++ ex29/ex29.rb
@@ -2,7 +2,11 @@ people = 20
 cats = 30
 dogs = 15
 
-puts 'Too many cats! The world is doomed!' if people < cats
+puts 'Too many cats! The world is doomedA!' if people < cats
+puts 'Too many cats! The world is doomedA1!' if people != cats
+puts 'Too many cats! The world is doomedB!' unless people > cats # if not = unless
+puts 'Too many cats! The world is doomedC!' if people && cats ## and 就是&&
+puts 'Too many cats! The world is doomedD!' if people || cats ## or 就是 ||
 puts 'PEACE' if people > cats
 puts 'The world is drooled on' if people < dogs
 puts 'The world is dry' if people > dogs
