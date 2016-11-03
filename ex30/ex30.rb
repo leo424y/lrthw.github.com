aca76cccbf2ab39e075742577355276c15dbb385
leo424y <leo424y@gmail.com>
2016-11-03 16:48:43 +0800

    ex30 gogo

--- ex30/ex30.rb
+++ ex30/ex30.rb
@@ -2,16 +2,16 @@ leo = 29
 grace = 30
 mom = 57
 
-if grace > leo
-  puts 'grace'
+if grace > leo && mom > leo
+  puts 'grace and mom'
 elsif grace < leo
   puts 'leo'
 else
   puts 'ha'
 end
 
-if mom > grace
-  puts 'mom'
+if mom > grace || grace < leo
+  puts 'mommmmmy'
 elsif mom < grace
   puts 'gramom'
 else
