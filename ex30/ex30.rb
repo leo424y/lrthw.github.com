de72ca9d3bd2ddccb55f5124299812d0c27d4db7
leo424y <leo424y@gmail.com>
2016-11-03 16:47:08 +0800

    ex30

---
+++ ex30/ex30.rb
@@ -0,0 +1,25 @@
+leo = 29
+grace = 30
+mom = 57
+
+if grace > leo
+  puts 'grace'
+elsif grace < leo
+  puts 'leo'
+else
+  puts 'ha'
+end
+
+if mom > grace
+  puts 'mom'
+elsif mom < grace
+  puts 'gramom'
+else
+  puts 'hahahah'
+end
+
+if leo > mom
+  puts 'leo'
+else
+  puts 'mom'
+end
