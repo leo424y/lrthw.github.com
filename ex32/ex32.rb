ff271be5a5bcd75f15710ea3ded5512b9a6dedfd
leo424y <leo424y@gmail.com>
2016-11-04 20:16:29 +0800

    cool string

--- ex32/ex32.rb
+++ ex32/ex32.rb
@@ -1,3 +1,7 @@
+def p(p)
+  puts p.to_s
+end
+leograce = %w(a b c d)
 the_count = [1, 2, 3, 4, 5]
 
 fruits = %w(apples oranges pears apricots)
@@ -20,11 +24,14 @@ end
 
 # also we can go through mixed arrays too
 
-for i in change
+# for i in change
+#
+#   puts "I got #{i}"
+#
+# end
 
-  puts "I got #{i}"
-
-end
+# 2
+change.each { |i| puts "I got #{i}" }
 
 # we can also build arrays, first start with an empty one
 
@@ -66,3 +73,47 @@ for j in elements
   puts "Element was: #{j}"
 
 end
+
+leograce.each { |x| puts x.to_s }
+p '第一' + leograce.first
+p '最後' + leograce.last
+p leograce.take(1)
+p leograce.take(2)
+p leograce.take(3)
+p leograce.drop(1)
+p leograce.drop(2)
+p leograce.length
+p '123'.length
+p leograce.count
+p leograce.insert(3, 'c1')
+p leograce.insert(3, 'c2')
+p leograce.push('d2')
+p leograce.pop
+p leograce << 'd3'
+p leograce.unshift 'a0'
+p leograce.shift
+p leograce.delete_at(1)
+p leograce.delete('a')
+p leograce << nil
+p leograce.compact
+p leograce
+p leograce.compact!
+p leograce
+p leograce.each { |a| print a += '10', ' ' }
+p leograce
+p leograce.reverse_each { |a| print a += '10', ' ' }
+p leograce
+p leograce.map { |a| a += '10' }
+p leograce
+p leograce.map! { |a| a += '10' }
+p leograce
+p leograce.select { |a| a == 'c10' }
+p leograce
+p leograce.reject { |a| a == 'c10' }
+p leograce
+p leograce.drop_while { |a| a == 'c10' }
+p leograce
+p leograce.delete_if { |a| a == 'c10' }
+p leograce
+p leograce.keep_if { |a| a == 'c210' }
+p leograce
