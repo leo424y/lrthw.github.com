70fcf1d46d95f201b0427fa43cc77f8d554b3890
leo424y <leo424y@gmail.com>
2016-11-09 11:01:02 +0800

    dit to go

--- next/pr.rb
+++ next/pr.rb
@@ -14,13 +14,21 @@ require 'uri'
 # word = '張'.dup.force_encoding('ASCII-8BIT')
 # p word帥
 #
+
+# 中文網址要做
 p final_url = URI.escape("https://www.moedict.tw/a/#{@word}.json")
+
+# 取回json
 response = HTTParty.get(final_url)
 
+# json 解開
 body = JSON.parse(response.body)
+
+# array 讀取 json
 p t = body['t'].gsub(/[~`]/, '')
 f = body['h'].each do |h|
   h['d'].each do |d|
+    # 去掉不要的字符
     p d['f'].gsub(/[~`]/, '')
   end
 end
