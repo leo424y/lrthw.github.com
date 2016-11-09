312278f95325ef43a8028056e1dc3d97bbdbd3ff
leo424y <leo424y@gmail.com>
2016-11-09 11:36:31 +0800

    404 OK

--- next/pr.rb
+++ next/pr.rb
@@ -3,25 +3,7 @@
 require 'httparty'
 require 'uri'
 
-@word = ARGV.first
-# 中文網址識別
-p final_url = URI.escape("https://www.moedict.tw/a/#{@word}.json")
-
-# 取回json
-
-# json 解開
-case response.body
-when 200
-  puts 'All good!'
-  response = HTTParty.get(final_url)
-  find_word
-when 404
-  puts 'O noes not found!'
-when 500...600
-  puts "ZOMG ERROR #{response.code}"
-end
-
-def find_word
+def find_word(body)
   # hash 讀取 json
   p t = body['t'].gsub(/[~`]/, '')
   f = body['h'].each do |h|
@@ -30,4 +12,30 @@ def find_word
       p d['f'].gsub(/[~`]/, '')
     end
   end
+end
+
+@word = ARGV.first
+# 中文網址識別
+p final_url = URI.escape("https://www.moedict.tw/a/#{@word}.json")
+
+# 取回json
+response = HTTParty.get(final_url)
+
+# json 解開
+# if response
+#   body = JSON.parse(response.body)
+#   find_word(body)
+# else
+#   p 'end'
+# end
+
+case response.code
+when 200
+  puts 'All good!'
+  body = JSON.parse(response.body)
+  find_word(body)
+when 404
+  puts 'O noes not found!'
+when 500...600
+  puts "ZOMG ERROR #{response.code}"
 end
