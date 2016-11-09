e8c5b35cc7d3ad2cdc79141ba86b49aee76bb0f7
leo424y <leo424y@gmail.com>
2016-11-09 11:31:58 +0800

    mix up

--- next/pr.rb
+++ next/pr.rb
@@ -4,21 +4,30 @@ require 'httparty'
 require 'uri'
 
 @word = ARGV.first
-
 # 中文網址識別
 p final_url = URI.escape("https://www.moedict.tw/a/#{@word}.json")
 
-# 取回json
-response = HTTParty.get(final_url)
+# 取回json
 
 # json 解開
-body = JSON.parse(response.body)
+case response.body
+when 200
+  puts 'All good!'
+  response = HTTParty.get(final_url)
+  find_word
+when 404
+  puts 'O noes not found!'
+when 500...600
+  puts "ZOMG ERROR #{response.code}"
+end
 
-# array 讀取 json
-p t = body['t'].gsub(/[~`]/, '')
-f = body['h'].each do |h|
-  h['d'].each do |d|
-    # 去掉不要的字符
-    p d['f'].gsub(/[~`]/, '')
+def find_word
+  # hash 讀取 json
+  p t = body['t'].gsub(/[~`]/, '')
+  f = body['h'].each do |h|
+    h['d'].to_a.each do |d|
+      # 去掉不要的字符
+      p d['f'].gsub(/[~`]/, '')
+    end
   end
 end
