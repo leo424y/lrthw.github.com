ced28c09884bc4d9d130453ddddaa9a0bac96cc1
leo424y <leo424y@gmail.com>
2016-11-09 11:01:13 +0800

    dictionary to go

--- next/pr.rb
+++ next/pr.rb
@@ -4,21 +4,11 @@ require 'httparty'
 require 'uri'
 
 @word = ARGV.first
-# final_url = URI.escape("https://www.moedict.tw/a/#{@word}.json")
-# p search_word = word.dup.force_encoding('ASCII-8BIT')
-# p search_word.class
-# p 'hello'.gsub(/[a-z]/, '%')
-# p search_word.gsub(/[a-z]/, '%')
-# p search_word.each_char { |c| print c, ' ' }
-# search_word.gsub(/[B]/, '1')
-# word = '張'.dup.force_encoding('ASCII-8BIT')
-# p word帥
-#
-
-# 中文網址要做
+
+# 中文網址識別
 p final_url = URI.escape("https://www.moedict.tw/a/#{@word}.json")
 
-# 取回json
+# 取回json
 response = HTTParty.get(final_url)
 
 # json 解開
